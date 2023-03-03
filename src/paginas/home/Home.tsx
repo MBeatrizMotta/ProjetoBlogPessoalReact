e8f8546bbb8286a5c7fadc5696ab6e5b2f02155f
MBeatrizMotta <mariabeatrizmottapino@gmail.com>
2023-03-02 23:58:53 -0300

    react 2

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+
+    <>
+    <h1 className="titulo">O mundo de Harry Potter</h1>
+    <img src="https://images7.alphacoders.com/119/1192080.jpg" alt="O mundo de Harry Potter" className="img" />
+    </>
+    );
+
+}
+
+export default Home;
