e880d9158d9dd3c905cd6ddacf32f6ac5c5c9f9d
MBeatrizMotta <mariabeatrizmottapino@gmail.com>
2023-03-06 02:10:19 -0300

    react 3

---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,24 @@
+import { Grid, Box } from '@material-ui/core';
+import React from 'react';
+import './Login.css';
+
+function Login() {
+
+return (
+   <Grid container>
+    <Grid xs={6}>
+        <Box>
+            <form>
+
+            </form>
+        </Box>
+    </Grid>
+    <Grid xs={6}>
+
+    </Grid>
+   </Grid>
+);
+
+}
+
+export default Login;
