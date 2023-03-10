58db702457806f865dcc53c4e732e8d3dbbdd52f
MBeatrizMotta <mariabeatrizmottapino@gmail.com>
2023-03-10 00:29:46 -0300

    react 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-import Home from './paginas/home/Home';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Login from './paginas/login/Login';
-import Footer from './components/estaticos/footer/Footer';
+import Home from "./paginas/home/Home"
+import Navbar from "./components/estaticos/navbar/Navbar"
+import Footer from "./components/estaticos/footer/Footer"
 import './App.css';
+import Login from "./paginas/login/Login"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
-      <Router>
+
+    <Router>
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
-      <Routes> // Antigo Switch
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route/>
-      </Routes>
+        <Routes> //Antigo Switch
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+        
+        </Routes>
       </div>
       <Footer />
-      </Router>
-      )
-  
+    </Router>
+
+
+  );
 }
 
 export default App;
